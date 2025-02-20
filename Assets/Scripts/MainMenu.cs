1eff2543355c6284b6c839e07f2093479b2bd5dd
xINVINCIBLEx <shubham170905@gmail.com>
2025-02-20 06:40:31 +0530

    Added: Checkpoint, Scene Load and Instant Kill

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
 
-        SceneManager.LoadSceneAsync("GloomyForest");
+        SceneManager.LoadSceneAsync("WhiteSpace");
         
     }
     public void QuitGame()
